d32a96725619e4237e83e36439caa6150c0d249f
hamzabaali <hamza2fes@gmail.com>
2016-05-12 14:39:42 +0200

    Update file-test-cpd-2.php

--- src/file-test-cpd-2.php
+++ src/file-test-cpd-2.php
@@ -1,27 +1,16 @@
 <?php
 
-class Vide
+class Fizzbuzz6
 {
     private $start = 1;
     private $end = 100;
     protected $results = array();
-    /**
-     * Class Construct
-     *
-     * @param int $start
-     * @param int $end
-     */
     public function __construct($start = 1, $end = 100)
     {
         $this->setStart($start);
         $this->setEnd($end);
     }
-    /**
-     * setEnd
-     *
-     * @param $end
-     */
-     public function vidde()
+    public function vide()
     {
         echo"une fonction vide ";
     }
@@ -29,30 +18,24 @@ class Vide
     {
         $this->end = $end;
     }
-    /**
-     * setStart
-     *
-     * @param $start
-     */
     public function setStart($start)
     {
         $this->start = $start;
     }
-    
-    public function run()
+     public function run()
     {
         $counter = $this->start;
         while ($counter <= $this->end) {
-            $this->results[$counter] = $this->izVide($counter);
+            $this->results[$counter] = $this->izFizzBuzz($counter);
             $counter ++;
         }
         return $this->results;
     }
     
-    public function izVide($number)
+    public function izFizzBuzz($number)
     {
         if (($number % 3 == 0) && ($number % 5 == 0)) {
-            return "Vide";
+            return "FizzBuzz";
         }
         if ($number % 3 == 0) {
             return "Fizz";
