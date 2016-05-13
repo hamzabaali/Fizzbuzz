c9201959836f499e92c9c9373314b6bb3d367d36
hamzabaali <hamza2fes@gmail.com>
2016-05-13 16:56:36 +0200

    Update Fizzbuzz.php

--- src/Fizzbuzz.php
+++ src/Fizzbuzz.php
@@ -101,10 +101,10 @@ private $q=15;
 
 
 
-    public static function main( array $as ) { // VIOLATION - Formal
+    public function maine( array $as ) { // VIOLATION - Formal
         $number = 20 + $this->q; // VIOLATION - Local
-        for (int $i = 0; $i < 10; $i++) { // Not a Violation (inside FOR)
-            $number += $this->q;
+        for (int $i=0;$i < 10;$i++) { // Not a Violation (inside FOR)
+            $number += $this->$q;
         }
     }
     
