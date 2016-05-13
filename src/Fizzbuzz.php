b461f14b05674b21e1732a45b2c0ecdfa442198f
hamzabaali <hamza2fes@gmail.com>
2016-05-13 16:46:06 +0200

    Update Fizzbuzz.php

--- src/Fizzbuzz.php
+++ src/Fizzbuzz.php
@@ -99,17 +99,7 @@ class Fizzbuzz{private $start = 1;private $end = 100;protected $results = array(
     }
 
 
-public function test()
-    {
 
-    private $q = 15; // VIOLATION - Field
-    public static function main( array $as ) { // VIOLATION - Formal
-        $r = 20 + $this->q; // VIOLATION - Local
-        for (int $i = 0; $i < 10; $i++) { // Not a Violation (inside FOR)
-            $r += $this->q;
-        }
-    }
-    }
       
 
 }
